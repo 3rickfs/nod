6408a3dd77f38a33cc4afbe1bcf7b0ef65fc5828
Erick Fiestas <efiestass@upao.edu.pe>
2023-12-18 12:22:31 -0500

    change flask api to be exposed to 0.0.0.0

--- nod/nod_api.py
+++ nod/nod_api.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    port = os.getenv('FLASK_PORT', '5000')
+    app.run(host=host, port=int(port))

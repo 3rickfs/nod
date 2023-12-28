5146d3ec45f2a1cedc6237df5bc4ac56aecccc14
Erick Fiestas <efiestass@upao.edu.pe>
2023-12-27 19:34:04 -0500

    Test 2 to get a ia model in json still failing

--- nod/test/pynister.py
+++ nod/test/pynister.py
@@ -19,75 +19,117 @@ def save_nod_info_in_dict(nod_id, capa_id, input_names, pesos, biases, fas,
     jsn["pesos"] = pesos
     jsn["biases"] = biases
     jsn["fas"] = fas
-    jsn["output_names"] = [
-        [i,j] for i, j in zip(output_names[0][:], output_names[1][:])
-    ][0] # making it just one array.
-    jsn["output_ep"] = [output_ep for i in range(len(output_names))]
+    #jsn["output_names"] = [
+    #    [i,j] for i, j in zip(output_names[0][:], output_names[1][:])
+    #][0] # making it just one array.
+    jsn["output_names"] = output_names
+    jsn["output_ep"] = [output_ep[0] for i in range(len(output_names))]
 
     return jsn
+
+def get_nods_number(neuron_num, neurons_per_nod):
+    div = neuron_num % neurons_per_nod
+    if div == 0:
+        nods_num = int(neuron_num / neurons_per_nod)
+    else:
+        nods_num = int(neuron_num / neurons_per_nod) + 1
+
+    return nods_num
+
+def read_endpoints(fpath):
+    #reading endpoints
+    eps = []
+    with open("./nod_endpoints.txt", "r") as ep_file:
+        eps = [line.rstrip() for line in ep_file]
+        #while True:
+        #    ep = ep_file.readline()
+        #    if not ep:
+        #        break
+        #    else:
+        #        eps.append(ep)
+        ep_file.close()
+    return eps
 
 @app.route("/model_to_neuron_sets", methods = ['POST'])
 def model_to_neuron_sets():
     json_data = request.get_json()
-
+    #split parameters model:
+    neurons_per_nod = json_data["CoN_parameters"]["neurons_per_nod"]
+    nod_num = json_data["CoN_parameters"]["nod_num"]
+    #this will be used for the neuro orchestrator to send json to nods too
+    nod_ep = read_endpoints("./nod_endpoints.txt")
+    neuro_orchestrator_ep = ["http://localhost:5000/final_prediction"]
     info_layer = []
     # get layers info:
-    nc = 1
+    nc = 1 #neurons counter
+    noc = 0 #nod counter
     for l in range(1, len(json_data["layers"])+1):
         layer_dict = json_data["layers"]["layer_" + str(l)]
-        ws, bs, fas, ins, ons = [], [], [], [], []
-        for n in range(1, len(layer_dict)+1):
+
+        neuron_num = len(layer_dict)
+        nods_num = get_nods_number(neuron_num, neurons_per_nod) 
+
+        ws, bs, fas, ins, ons, eps = [], [], [], [], [], []
+        for n in range(1, neuron_num+1):
             neuron_dict = layer_dict["neuron_" + str(nc)]
             ws.append(neuron_dict["pesos"]["w"])
             bs.append(neuron_dict["bias"])
             fas.append(neuron_dict["fa"])
             ins.append(neuron_dict["inputs_names"])
-            ons.append(neuron_dict["outputs_names"])
+            ons.append(neuron_dict["outputs_names"][0])
             nc += 1
+        #list of NOD endpoints in current layer
+        eps = nod_ep[noc:noc+nods_num]
+        noc += nods_num
 
-        info_layer.append([ins, ws, bs, fas, ons])
-
-    #split model:
-    neurons_per_nod = json_data["CoN_parameters"]["neurons_per_nod"]
-    nod_num = json_data["CoN_parameters"]["nod_num"]
-    nod_ep = ["http://localhost:5000/final_prediction",
-              "http://a545ed66e46bc4ea788f4aada7bcea72-1944961211."+\
-              "us-west-1.elb.amazonaws.com:5000/exec_neurons",
-              "http://a6823b10e5c7a4dd5a038a6f813091b4-242539242."+\
-              "us-west-1.elb.amazonaws.com:5000/exec_neurons"]
+        info_layer.append([ins, ws, bs, fas, ons, eps])
 
     nod_dict = {}
     #last layer just one neuron
     layers_num = len(info_layer)
-    nc = 1
+    noc = 1
     for layer in range(layers_num):
-        neuron_num = len(info_layer[layer])
-        nods_num = int(neuron_num / neurons_per_nod)
         if layer == layers_num - 1: #last layer
-            nod_dict["nod_" + str(nc)] = save_nod_info_in_dict(
-                nc,
+            nod_dict["nod_" + str(noc)] = save_nod_info_in_dict(
+                noc,
                 layer + 1,
                 info_layer[layer][0],
                 info_layer[layer][1],
                 info_layer[layer][2],
                 info_layer[layer][3],
                 info_layer[layer][4],
-                nod_ep[0] #to the pynister
+                neuro_orchestrator_ep #to the pynister
             )
-            nc = 1
+            noc = 1
         else:
-            for j in range(0, len(info_layer[layer][2]), nods_num):
-                nod_dict["nod_" + str(nc)] = save_nod_info_in_dict(
-                    nc,
+            #for j in range(0, len(info_layer[layer][2]), #nods_num):
+            neuron_num = len(info_layer[layer][2])
+            nods_num = len(info_layer[layer][5]) #int(neuron_num / neurons_per_nod)
+            for j in range(0, neuron_num, neurons_per_nod):
+                nod_dict["nod_" + str(noc)] = save_nod_info_in_dict(
+                    noc,
                     layer + 1,
                     info_layer[layer][0],
-                    info_layer[layer][1][j:j+neuron_num],
-                    info_layer[layer][2][j:j+neuron_num],
-                    info_layer[layer][3][j:j+neuron_num],
+                    info_layer[layer][1][j:j+neurons_per_nod], #neuron_num],
+                    info_layer[layer][2][j:j+neurons_per_nod],  #neuron_num],
+                    info_layer[layer][3][j:j+neurons_per_nod], #neuron_num],
                     info_layer[layer][4],
-                    nod_ep[nc + 1] #to the next neuron
+                    info_layer[layer+1][5] #ep to the next neuron in next layer
                 )
-            nc += 1
+                nods_num -= 1 #every time it's added a new nod
+            noc += 1
+            if nods_num > 0:
+                #it left one nod to be added, there're more neurons yet
+                nod_dict["nod" + str(noc)] = save_nod_info_in_dict(
+                    noc,
+                    layer + 1,
+                    info_layer[layer][0],
+                    info_layer[layer][1][j:],
+                    info_layer[layer][2][j:],
+                    info_layer[layer][3][j:],
+                    info_layer[layer][4],
+                    info_layer[layer+1][5] #ep to the next neuron in next layer
+                )
 
     return json.dumps(nod_dict) #json_data['model_info']
 
--- nod/test/test_api_endpoints.py
+++ nod/test/test_api_endpoints.py
@@ -100,6 +100,88 @@ class neuron_ops_test_cases(unittest.TestCase):
 
         print("*"*100)
 
+
+    def test_model_json_to_neuron_set_jsons_2(self):
+        print("*"*100)
+        print("Test 3: again call http://IP:PORT/model_to_neuron_sets to split the"
+               "whole model (another one) in json into group of neurons in json too.")
+        print("-------------------------------------------------------------")
+        input_file_name = "./test_dense_model_2.json"
+        CoN_parameters = {
+            "neurons_per_nod": 2,
+            "nod_num": 10
+        }
+        expected_result = {
+            "nod_1": {
+                "nod_id": "1",
+                "capa_id": "1",
+                "input_names": ['x1', 'x2', 'x3', 'x4', 'x5'],
+                "pesos": [
+                    [0.171137273311615, 0.17062145471572876, -0.23058700561523438, -0.2599382996559143, 0.12979340553283691],
+                    [0.02977389097213745, 0.2760855555534363, -0.6309725046157837, 0.581677258014679, -0.47849205136299133]
+                ],
+                "biases": [0.0, 0.0],
+                "fas": ['relu', 'relu'],
+                "output_names": ['o1', 'o2'],
+                "output_ep":[("http://a6823b10e5c7a4dd5a038a6f813091b4-"
+                              "242539242.us-west-1.elb.amazonaws.com:5000/"
+                              "exec_neurons")
+                            ]
+            },
+            "nod_2": {
+                "nod_id": "2",
+                "capa_id": "1",
+                "input_names": ['x1', 'x2', 'x3', 'x4', 'x5'],
+                "pesos": [
+                    [0.17120105028152466, 0.2488141655921936, -0.2648974061012268, 0.2123282551765442, -0.485383003950119]
+                ],
+                "biases": [0.0],
+                "fas": ['relu'],
+                "output_names": ['o3'],
+                "output_ep":[("http://a6823b10e5c7a4dd5a038a6f813091b4-"
+                              "242539242.us-west-1.elb.amazonaws.com:5000/"
+                              "exec_neurons")
+                            ]
+            },
+            "nod_3": {
+                "nod_id": "3",
+                "capa_id": "2",
+                "input_names": ['o1', 'o2', 'o3'],
+                "pesos": [
+                    [0.0460209846496582, 0.21512341499328613, 0.16031408309936523]
+                ],
+                "biases": [0.0],
+                "fas": ['relu'],
+                "output_names": ['o4'],
+                "output_ep":["http://localhost:5000/final_prediction"]
+            }
+        }
+
+        try:
+            with open(input_file_name, 'rb') as jf:
+                input_json_file = json.load(jf)
+            #print(f"json file: {input_json_file}")
+            #adding parameters of Cluster of Neurons
+            input_json_file["CoN_parameters"] = CoN_parameters
+            json_data = json.dumps(input_json_file)
+            headers = {'Content-type': 'application/json'}
+            result = requests.post("http://localhost:5000/model_to_neuron_sets",
+                                   data=json_data, headers=headers
+                                  )
+
+            print("__________________________________")
+            print(f"result: {json.loads(result.text)}")
+            print("__________________________________")
+            print(f"expected_result: {expected_result}")
+            self.assertEqual(expected_result, json.loads(result.text))
+        except Exception as e:
+            print("%"*100)
+            print(f"error: {e}")
+            print("%"*100)
+
+        print("*"*100)
+
+
 if __name__ == '__main__':
     unittest.main()
 

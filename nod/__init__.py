cd47f0f2f418355a017366e7a0460003b3c28f75
Erick Fiestas <efiestass@upao.edu.pe>
2024-02-01 00:53:48 -0500

    removing import module from init

--- nod/__init__.py
+++ nod/__init__.py
@@ -2,6 +2,6 @@
 NOD (Neuro Orchestrated device) runs neurons, basic units of Deep Learning models. Neurons are distribuited along the network of NODs, Nodes and Servers in Cloud.
 """
 
-from . import nod
+#from . import nod
 
 __version__ = '1.0.0'
